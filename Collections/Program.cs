74ebc58fed67eeda6889c97eb3cff9184235936b
Sistemas2-23 <Sistemas2-23@DESKTOP-97G4FJ2>
2023-10-10 12:03:52 -0600

    Agregar archivos de proyecto.

---
+++ Collections/Program.cs
@@ -0,0 +1,53 @@
+﻿// Crear una coleccion generica de 5 frutas 
+
+// Imprimir o desplegar la informacion de la coleccion (elementos, tamaño)
+
+//Buscar en la coleccion la fruta naranja
+
+// Insertar por indice una nueva fruta 
+
+//Remover la fruta insertada y remover la fruta con el indice [0]
+
+//Imprimir 
+
+
+using System.Collections.ObjectModel;
+
+Collection<string> Frutas = new Collection<string>();
+
+Frutas.Add("Manzana");
+Frutas.Add("Mango");
+Frutas.Add("Sandía");
+Frutas.Add("Fresa");
+Frutas.Add("Piña");
+
+Console.WriteLine("{0} Frutas: ", Frutas.Count);
+Display(Frutas);
+
+Console.WriteLine("\n La colección contiene(\"Naranja\"):{0}", Frutas.Contains("Naranja"));
+
+Console.WriteLine("\n Se ha agragado(2, \"Pera\")");
+Frutas.Insert(2, "Pera"); 
+Display(Frutas);
+
+Console.WriteLine("\nRemoveAt(2)");
+Frutas.RemoveAt(2);
+Display(Frutas);
+
+Console.WriteLine("\nRemoveAt(0)");
+Frutas.RemoveAt(0);
+Display(Frutas);
+
+
+
+
+static void Display(Collection<string> frutas)
+{
+    Console.WriteLine();
+
+    foreach (string fr in frutas) {
+
+        Console.WriteLine(fr);
+    
+    }
+}
---
+++ PruebaList/Program.cs
@@ -0,0 +1,45 @@
+﻿string[] colores = { "Magneta ", "Rojo ", "Blanco ", "Azul ", "Cyan " };
+
+string[] eliminaColores = { "Rojo ", "Blanco", "Azul " }; 
+
+List<string> list = new List<string>();
+
+foreach ( string item in colores)
+{
+    list.Add(item); 
+}
+
+List<string> List2 = new List<string>(eliminaColores);
+
+MostrarInformacion(list);
+EliminarColores(list, eliminaColores);
+
+MostrarInformacion(list); 
+
+
+void MostrarInformacion(List<string> list)
+{
+    foreach (var item in list) {
+
+        Console.WriteLine($"{item} ");
+    
+    }
+
+    Console.WriteLine($"\nTamaño = {list.Count}; " + $"Capacidad = {list.Capacity}");
+
+    int index = list.IndexOf("Azul");
+
+    if (index != -1) Console.WriteLine("La lista contiene Azul " + "en el indice {0}", index);
+
+    else Console.WriteLine("No contiene Azul" );
+
+}
+
+
+void EliminarColores(List<string> list, string[] eliminaColores)
+{
+    foreach (var item in eliminaColores)
+    {
+        list.Remove(item);
+    }
+}
